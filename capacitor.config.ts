19f290fa3c182bf54a5dd6a2abdeea7db34a0ae7
Osmel Mendez <mdz99h@gmail.com>
2023-08-11 18:57:47 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Atención Ciudadana',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
