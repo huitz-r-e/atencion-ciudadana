99f3e1da4777d6db24e173f5c512ad32ada520aa
Osmel Mendez <84472632+OsmelMdz@users.noreply.github.com>
2023-08-14 00:23:04 -0600

    Tabs1

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
+  async showAlert() {
+    const alert = await this.alertController.create({
+      header: '¡Auxilio Enviado!',
+      message: 'Tu auxilio ha sido enviado correctamente.',
+     // buttons: ['Aceptar'],
+    });
+
+    await alert.present();
+  }
 
 }
